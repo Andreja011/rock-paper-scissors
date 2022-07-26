567ae2ba9b4fa04d06ebd59eebabb6b71066b918
Andreja011 <andrejasimonovic011@gmail.com>
2022-07-26 19:49:20 +0200

    Fixing formating

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 "use strict";
 
-// Getting player choise
-// const playerChoise = prompt("What is your choise?").toLowerCase();
-
-// // Getting computer random choise
-// const computerChoise = function () {
-//   const items = ["Rock", "Paper", "Scissor"];
-//   const choise = items[Math.floor(Math.random() * items.length)].toLowerCase();
-//   return choise;
-// };
-
 // Scores
 let rounds = 0;
 let playerScore = 0;
@@ -25,27 +15,34 @@ const playRound = function (player, computer) {
   // Rock
   if (player === "rock" && computer === "paper") {
     computerScore++;
-    return `You lose. ${playerCapitalize} is weaker than ${computerCapitalize}. Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    return `You lose. ${playerCapitalize} is weaker than ${computerCapitalize}. 
+    Score is: Player ${playerScore}, Computer ${computerScore}.`;
   } else if (player === "rock" && computer === "scissor") {
     playerScore++;
-    return `You win. ${playerCapitalize} is stronger than ${computerCapitalize}. Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    return `You win. ${playerCapitalize} is stronger than ${computerCapitalize}.
+    Score is: Player ${playerScore}, Computer ${computerScore}.`;
     // Paper
   } else if (player === "paper" && computer === "rock") {
     playerScore++;
-    return `You win. ${playerCapitalize} is stronger than ${computerCapitalize}. Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    return `You win. ${playerCapitalize} is stronger than ${computerCapitalize}.
+    Score is: Player ${playerScore}, Computer ${computerScore}.`;
   } else if (player === "paper" && computer === "scissor") {
     computerScore++;
-    return `You lose. ${playerCapitalize} is weaker than ${computerCapitalize}. Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    return `You lose. ${playerCapitalize} is weaker than ${computerCapitalize}. 
+    Score is: Player ${playerScore}, Computer ${computerScore}.`;
     // Scissor
   } else if (player === "scissor" && computer === "paper") {
     playerScore++;
-    return `You win. ${playerCapitalize} is stronger than ${computerCapitalize}. Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    return `You win. ${playerCapitalize} is stronger than ${computerCapitalize}.
+    Score is: Player ${playerScore}, Computer ${computerScore}.`;
   } else if (player === "scissor" && computer === "rock") {
     computerScore++;
-    return `You lose. ${playerCapitalize} is weaker than ${computerCapitalize}. Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    return `You lose. ${playerCapitalize} is weaker than ${computerCapitalize}. 
+    Score is: Player ${playerScore}, Computer ${computerScore}.`;
     // Draw
   } else
-    return `Draw. ${playerCapitalize} and ${computerCapitalize} are the same. Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    return `Draw. ${playerCapitalize} and ${computerCapitalize} are the same. 
+    Score is: Player ${playerScore}, Computer ${computerScore}.`;
 };
 
 const game = function () {
