6618739b5c5336f74d1c1761e850573914da4e72
Andreja011 <andrejasimonovic011@gmail.com>
2022-08-18 22:35:53 +0200

    Added css file and some minor changes in html

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ const playRound = function (player, computer) {
 const game = function () {
   for (let i = rounds; rounds < 5; rounds++) {
     // Getting player choise
-    const playerChoise = prompt("What is your choise?").toLowerCase();
+    // const playerChoise = prompt("What is your choise?").toLowerCase();
 
     // Getting computer random choise
     const computerChoise = function () {
@@ -67,3 +67,17 @@ const game = function () {
 };
 
 console.log(game());
+
+// New Excersise
+// Create three buttons, one for each selection.
+
+// Add an event listener to the buttons that call your playRound function
+// with the correct playerSelection every time a button is clicked.
+// (you can keep the console.logs for this step)
+// Add a div for displaying results
+// and change all of your console.logs into DOM methods.
+// Display the running score,
+// and announce a winner of the game once one player reaches 5 points.
+// You will likely have to refactor (rework/rewrite) your original code
+// to make it work for this.
+// That’s OK! Reworking old code is an important part of a programmer’s life.
