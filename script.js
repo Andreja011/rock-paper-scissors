9669b717bcd723f211ab0cce46089e767cd06e6b
Andreja011 <andrejasimonovic011@gmail.com>
2022-09-05 21:01:59 +0200

    Added eventlisteners on buttons

--- script.js
+++ script.js
@@ -5,65 +5,80 @@ let rounds = 0;
 let playerScore = 0;
 let computerScore = 0;
 
+const rock = document.querySelector(`.rock`);
+const paper = document.querySelector(`.paper`);
+const scissor = document.querySelector(`.scissor`);
+
+rock.addEventListener("click", function () {
+  console.log(playRound("rock", computerChoise()));
+});
+paper.addEventListener("click", function () {
+  console.log(playRound("rock", computerChoise()));
+});
+scissor.addEventListener("click", function () {
+  console.log(playRound("scissor", computerChoise()));
+});
+const computerChoise = function () {
+  const items = ["Rock", "Paper", "Scissor"];
+  const choise = items[Math.floor(Math.random() * items.length)].toLowerCase();
+  return choise;
+};
 // Play round
 const playRound = function (player, computer) {
   // Capitalize first letter
-  const playerCapitalize = player.charAt(0).toUpperCase() + player.slice(1);
+  // const playerCapitalize = player.charAt(0).toUpperCase() + player.slice(1);
   const computerCapitalize =
     computer.charAt(0).toUpperCase() + computer.slice(1);
   // Comparation
   // Rock
   if (player === "rock" && computer === "paper") {
     computerScore++;
-    return `You lose. ${playerCapitalize} is weaker than ${computerCapitalize}. 
+    return `You lose. Rock is weaker than ${computerCapitalize}. 
     Score is: Player ${playerScore}, Computer ${computerScore}.`;
   } else if (player === "rock" && computer === "scissor") {
     playerScore++;
-    return `You win. ${playerCapitalize} is stronger than ${computerCapitalize}.
+    return `You win. Rock is stronger than ${computerCapitalize}.
     Score is: Player ${playerScore}, Computer ${computerScore}.`;
     // Paper
   } else if (player === "paper" && computer === "rock") {
     playerScore++;
-    return `You win. ${playerCapitalize} is stronger than ${computerCapitalize}.
+    return `You win. Paper is stronger than ${computerCapitalize}.
     Score is: Player ${playerScore}, Computer ${computerScore}.`;
   } else if (player === "paper" && computer === "scissor") {
     computerScore++;
-    return `You lose. ${playerCapitalize} is weaker than ${computerCapitalize}. 
+    return `You lose. Paper is weaker than ${computerCapitalize}. 
     Score is: Player ${playerScore}, Computer ${computerScore}.`;
     // Scissor
   } else if (player === "scissor" && computer === "paper") {
     playerScore++;
-    return `You win. ${playerCapitalize} is stronger than ${computerCapitalize}.
+    return `You win. Scissor is stronger than ${computerCapitalize}.
     Score is: Player ${playerScore}, Computer ${computerScore}.`;
   } else if (player === "scissor" && computer === "rock") {
     computerScore++;
-    return `You lose. ${playerCapitalize} is weaker than ${computerCapitalize}. 
+    return `You lose. Scissor is weaker than ${computerCapitalize}. 
     Score is: Player ${playerScore}, Computer ${computerScore}.`;
     // Draw
-  } else
-    return `Draw. ${playerCapitalize} and ${computerCapitalize} are the same. 
+  } else if (player === computer) {
+    return `Draw. ${
+      player.charAt(0).toUpperCase() + player.slice(1)
+    } and ${computerCapitalize} are the same. 
     Score is: Player ${playerScore}, Computer ${computerScore}.`;
+  }
 };
 
 const game = function () {
   for (let i = rounds; rounds < 5; rounds++) {
     // Getting player choise
     // const playerChoise = prompt("What is your choise?").toLowerCase();
-
     // Getting computer random choise
-    const computerChoise = function () {
-      const items = ["Rock", "Paper", "Scissor"];
-      const choise =
-        items[Math.floor(Math.random() * items.length)].toLowerCase();
-      return choise;
-    };
-    console.log(playRound(playerChoise, computerChoise()));
+    // console.log(playRound(playerChoise, computerChoise()));
+
+    if (playerScore === computerScore) {
+      return "Tie.";
+    } else if (playerScore > computerScore) {
+      return "You won!";
+    } else return "Computer win!";
   }
-  if (playerScore === computerScore) {
-    return "Tie.";
-  } else if (playerScore > computerScore) {
-    return "You won!";
-  } else return "Computer win!";
 };
 
 console.log(game());
