7220bf38ff0c267c0b956e09036774809adac001
Andreja011 <andrejasimonovic011@gmail.com>
2022-09-10 21:17:41 +0200

    Added scores in score box and message in message box

--- script.js
+++ script.js
@@ -4,26 +4,35 @@
 let rounds = 0;
 let playerScore = 0;
 let computerScore = 0;
+let totalScore = playerScore + computerScore;
 
 const rock = document.querySelector(`.rock`);
 const paper = document.querySelector(`.paper`);
 const scissor = document.querySelector(`.scissor`);
 const messages = document.querySelector(`.section-player-scores-result`);
+const scorePlayer = document.querySelector(
+  `.section-player-scores-card-score.player`
+);
+const scoreComputer = document.querySelector(
+  `.section-player-scores-card-score.computer`
+);
 
 rock.addEventListener("click", function () {
-  console.log(playRound("rock", computerChoise()));
+  playRound("rock", computerChoise());
 });
 paper.addEventListener("click", function () {
-  console.log(playRound("rock", computerChoise()));
+  playRound("paper", computerChoise());
 });
 scissor.addEventListener("click", function () {
-  console.log(playRound("scissor", computerChoise()));
+  playRound("scissor", computerChoise());
 });
+
 const computerChoise = function () {
   const items = ["Rock", "Paper", "Scissor"];
   const choise = items[Math.floor(Math.random() * items.length)].toLowerCase();
   return choise;
 };
+
 // Play round
 const playRound = function (player, computer) {
   // Capitalize first letter
@@ -34,59 +43,86 @@ const playRound = function (player, computer) {
   // Rock
   if (player === "rock" && computer === "paper") {
     computerScore++;
-    const playerLoseRock = `You lose. Rock is weaker than ${computerCapitalize}.
-    Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    const playerLoseRock = `You lose. Rock is weaker than ${computerCapitalize}.`;
     messages.classList.add("messages");
-    return (messages.textContent = playerLoseRock);
+    return (
+      (scorePlayer.textContent = playerScore),
+      (scoreComputer.textContent = computerScore),
+      (messages.textContent = playerLoseRock)
+    );
   } else if (player === "rock" && computer === "scissor") {
     playerScore++;
-    const playerWinRock = `You win. Rock is stronger than ${computerCapitalize}.
-    Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    const playerWinRock = `You win. Rock is stronger than ${computerCapitalize}.`;
     messages.classList.add("messages");
-    return (messages.textContent = playerWinRock);
+    return (
+      (scorePlayer.textContent = playerScore),
+      (scoreComputer.textContent = computerScore),
+      (messages.textContent = playerWinRock)
+    );
     // Paper
   } else if (player === "paper" && computer === "rock") {
     playerScore++;
-    return `You win. Paper is stronger than ${computerCapitalize}.
-    Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    const playerWinPaper = `You win. Paper is stronger than ${computerCapitalize}.`;
+    messages.classList.add("messages");
+    return (
+      (scorePlayer.textContent = playerScore),
+      (scoreComputer.textContent = computerScore),
+      (messages.textContent = playerWinPaper)
+    );
   } else if (player === "paper" && computer === "scissor") {
     computerScore++;
-    return `You lose. Paper is weaker than ${computerCapitalize}. 
-    Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    const playerLosePaper = `You lose. Paper is weaker than ${computerCapitalize}.`;
+    messages.classList.add("messages");
+    return (
+      (scorePlayer.textContent = playerScore),
+      (scoreComputer.textContent = computerScore),
+      (messages.textContent = playerLosePaper)
+    );
     // Scissor
   } else if (player === "scissor" && computer === "paper") {
     playerScore++;
-    return `You win. Scissor is stronger than ${computerCapitalize}.
-    Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    const playerWinScissor = `You win. Scissor is stronger than ${computerCapitalize}.`;
+    messages.classList.add("messages");
+    return (
+      (scorePlayer.textContent = playerScore),
+      (scoreComputer.textContent = computerScore),
+      (messages.textContent = playerWinScissor)
+    );
   } else if (player === "scissor" && computer === "rock") {
     computerScore++;
-    return `You lose. Scissor is weaker than ${computerCapitalize}. 
-    Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    const playerLoseScissor = `You lose. Scissor is weaker than ${computerCapitalize}. `;
+    messages.classList.add("messages");
+    return (
+      (scorePlayer.textContent = playerScore),
+      (scoreComputer.textContent = computerScore),
+      (messages.textContent = playerLoseScissor)
+    );
     // Draw
   } else if (player === computer) {
-    return `Draw. ${
+    const draw = `Draw. ${
       player.charAt(0).toUpperCase() + player.slice(1)
-    } and ${computerCapitalize} are the same. 
-    Score is: Player ${playerScore}, Computer ${computerScore}.`;
+    } and ${computerCapitalize} are the same.`;
+    messages.classList.add("messages");
+    return (messages.textContent = draw);
   }
 };
 
-const game = function () {
-  for (let i = rounds; rounds < 5; rounds++) {
-    // Getting player choise
-    // const playerChoise = prompt("What is your choise?").toLowerCase();
-    // Getting computer random choise
-    // console.log(playRound(playerChoise, computerChoise()));
+// const game = function () {
+//   for (let i = rounds; rounds < 5; rounds++) {
+//     // Getting player choise
+//     // const playerChoise = prompt("What is your choise?").toLowerCase();
+//     // Getting computer random choise
+//     // console.log(playRound(playerChoise, computerChoise()));
 
-    if (playerScore === computerScore) {
-      return "Tie.";
-    } else if (playerScore > computerScore) {
-      return "You won!";
-    } else return "Computer win!";
-  }
-};
+//     if (playerScore === computerScore) {
+//       return "Tie.";
+//     } else if (playerScore > computerScore) {
+//       return "You won!";
+//     } else return "Computer win!";
+//   }
+// };
 
-console.log(game());
+// console.log(game());
 
 // New Excersise
 // Create three buttons, one for each selection.
